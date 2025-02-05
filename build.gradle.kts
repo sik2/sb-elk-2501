76e0d5a24897137872cadfc1d5f0617ce82936dd
sik2 <bickwang123@gmail.com>
2025-02-05 10:21:02 +0900

    logback

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,9 @@ dependencies {
 
     runtimeOnly("com.mysql:mysql-connector-j")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // 로그백
+    implementation("net.logstash.logback:logstash-logback-encoder:7.4")
 }
 
 tasks.withType<Test> {
--- src/main/java/com/ll/elk/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/elk/domain/home/home/controller/HomeController.java
@@ -1,12 +1,41 @@
 package com.ll.elk.domain.home.home.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
+@Slf4j
 public class HomeController {
+
         @GetMapping("/")
         public String home() {
+            LocalDateTime localDateTime = LocalDateTime.now();
+            log.info("Home " + localDateTime);
             return "Hello, World!";
         }
+
+    @GetMapping("/logs")
+    public String Logs() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        log.info("Logs " + localDateTime);
+        return "Logs page";
+    }
+
+    @GetMapping("/warn")
+    public String Warn() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        log.warn("Warn" + localDateTime);
+        return "Warn page";
+    }
+
+
+    @GetMapping("/err")
+    public String Error() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        log.error("Error " + localDateTime);
+        return "Error page";
+    }
 }
